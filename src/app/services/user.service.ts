6e0822c9c48706c5e10573370ae88411fbaff8a3
Andrej Suba <103282953+AndrejSub@users.noreply.github.com>
2024-04-27 13:42:54 +0200

    Changes to  user email

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(
+      private httpClient: HttpClient)
+  { }
+
+
+  deleteUserByEmail(email: string){
+    this.httpClient.delete(`http://localhost:8222/users/email=${email}`);
+  }
+}

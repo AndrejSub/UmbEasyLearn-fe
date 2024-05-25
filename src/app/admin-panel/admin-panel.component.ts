e63019b0c75025c44d06ac51e0d2449107a47288
Andrej Suba <103282953+AndrejSub@users.noreply.github.com>
2024-05-25 14:48:00 +0200

    Fix to navbar plus added some feedback when adding test

--- src/app/admin-panel/admin-panel.component.ts
+++ src/app/admin-panel/admin-panel.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {LoginService} from "../services/login.service";
 
 @Component({
     selector: 'app-admin-panel',
@@ -8,5 +9,14 @@ import { CommonModule } from '@angular/common';
     templateUrl: './admin-panel.component.html',
     styleUrl: './admin-panel.component.scss'
 })
-export class AdminPanelComponent {
+export class AdminPanelComponent implements OnInit{
+    constructor(private loginService: LoginService) {
+    }
+
+    async ngOnInit(): Promise<any> {
+        if (await this.loginService.logIn()) {
+            this.loginService.logedIn= true;
+            console.log(this.loginService.logedIn);
+        }
+    }
 }
--- src/app/admin-panel/exam-form/exam-form.component.ts
+++ src/app/admin-panel/exam-form/exam-form.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from '@angular/core';
+import {Component, inject, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {AsyncPipe, NgForOf, NgIf} from "@angular/common";
 import {BehaviorSubject} from "rxjs";
@@ -7,6 +7,8 @@ import {AbcControlComponent} from "./abc-control/abc-control.component";
 import {SubjectsService} from "../../services/subjects.service";
 import {TestsService} from "../../services/tests.service";
 import {PraktickeControlComponent} from "./prakticke-control/prakticke-control.component";
+import {ToastrService} from "ngx-toastr";
+import {LoginService} from "../../services/login.service";
 
 @Component({
   selector: 'app-exam-form',
@@ -23,7 +25,7 @@ import {PraktickeControlComponent} from "./prakticke-control/prakticke-control.c
   templateUrl: './exam-form.component.html',
   styleUrl: './exam-form.component.scss'
 })
-export class ExamFormComponent {
+export class ExamFormComponent  implements OnInit {
   fb = inject(FormBuilder);
   form: FormGroup;
   saveTest$ = new BehaviorSubject(false);
@@ -39,14 +41,16 @@ export class ExamFormComponent {
 
   loading = false;
 
-  constructor() {
+  constructor(private toastr: ToastrService,
+              private loginService: LoginService) {
     this.form = this.fb.group({
       testName: new FormControl('', Validators.required),
       subjectID: new FormControl(''),
       flashCards: new FormControl([]),
       textQuestions: new FormControl([]),
       practicalQuestions: new FormControl([]),
-      abcQuestions: new FormControl([])
+      abcQuestions: new FormControl([]),
+
     })
   }
 
@@ -82,10 +86,20 @@ export class ExamFormComponent {
               this.flashCards = [];
               this.practicalQuestions = [];
               this.textQuestions = [];
+              this.toastr.success("Test bol pridaný ")
+            }, error =>{
+              this.loading = false
+        this.toastr.error("Aby si mohol pridávať testy musíš byť admin")
             }
         );
     },200)
 
+  }
+  async ngOnInit(): Promise<any> {
+    if (await this.loginService.logIn()) {
+      this.loginService.logedIn= true;
+      console.log(this.loginService.logedIn);
+    }
   }
 
 
